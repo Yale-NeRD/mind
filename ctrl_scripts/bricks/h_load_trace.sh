c01df36695a872ab685f03c3595be87118c1c660
Seung-seob Lee <shsym1989@gmail.com>
2021-08-24 18:42:29 -0400

    Bugfix | waiting for the sftp download in memory trace load script

--- ctrl_scripts/bricks/h_load_trace.sh
+++ ctrl_scripts/bricks/h_load_trace.sh
@@ -22,13 +22,13 @@ echo cd ${DST_DIR_1}/
 cd ${DST_DIR_1}/
 mkdir -p ~/sftp_logs
 for i in 0 1 2 3 4 5 6 7 8 9; do
-  sudo -b sftp -i ${SERVER_KEY} ${USER}@${LOG_SERVER}:${SRC_DIR}/partitioned/${PRE}$(((SERVER_ID-1)*20+i))${POST} > ~/sftp_logs/foo_${i}.out 2> ~/sftp_logs/foo_${i}.err < /dev/null &
+  sudo sftp -i ${SERVER_KEY} ${USER}@${LOG_SERVER}:${SRC_DIR}/partitioned/${PRE}$(((SERVER_ID-1)*20+i))${POST} > ~/sftp_logs/foo_${i}.out 2> ~/sftp_logs/foo_${i}.err < /dev/null &
   sleep 1
 done
 echo cd ${DST_DIR_2}/
 cd ${DST_DIR_2}/
 for i in 10 11 12 13 14 15 16 17 18 19; do
-  sudo -b sftp -i ${SERVER_KEY} ${USER}@${LOG_SERVER}:${SRC_DIR}/partitioned/${PRE}$(((SERVER_ID-1)*20+i))${POST} > ~/sftp_logs/foo_${i}.out 2> ~/sftp_logs/foo_${i}.err < /dev/null &
+  sudo sftp -i ${SERVER_KEY} ${USER}@${LOG_SERVER}:${SRC_DIR}/partitioned/${PRE}$(((SERVER_ID-1)*20+i))${POST} > ~/sftp_logs/foo_${i}.out 2> ~/sftp_logs/foo_${i}.err < /dev/null &
   sleep 1
 done
 # wait for sftp
