f694de44f04ac186455b4ecf685fe8b56505be52
yyppyy <yuyanpeng@pku.edu.cn>
2021-09-09 09:28:21 -0400

    reset vm only once

--- ctrl_scripts/scripts/run_commands.py
+++ ctrl_scripts/scripts/run_commands.py
@@ -146,7 +146,10 @@ def build_vm_start_command(server_ip, user, key, vm_name):
     return cmd
 
 def build_vm_reboot_command(server_ip, user, key, vm_name):
-    cmd = "sudo virsh reboot " + vm_name
+    #cmd = "sudo virsh reboot " + vm_name
+    cmd = "sudo virsh destroy " + vm_name + " && "
+    cmd += "sudo virsh start " + vm_name + " && "
+    cmd += "sleep 15" + " && "
     cmd = build_ssh_base(server_ip, user, key) + build_ssh_cmd(cmd)
     return cmd
 
@@ -157,7 +160,9 @@ def build_vm_shutdown_command(server_ip, user, key, vm_name):
 
 
 def build_vm_reset_command(server_ip, user, key, vm_name):
-    cmd = "sudo virsh reboot " + vm_name + " && "
+    #cmd = "sudo virsh reboot " + vm_name + " && "
+    cmd = "sudo virsh destroy " + vm_name + " && "
+    cmd += "sudo virsh start " + vm_name + " && "
     cmd += "sleep 15" + " && "
     cmd += "sudo virsh reset " + vm_name
     cmd = build_ssh_base(server_ip, user, key) + build_ssh_cmd(cmd)
