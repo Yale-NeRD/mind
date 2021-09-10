7fc102b07649d988d2e7d3aafb3b7837f861e5b2
Seung-seob Lee <seung-seob.lee@yale.edu>
2021-09-09 21:11:23 -0400

    Update 04macro_bench_res.sh

--- ctrl_scripts/scripts/post_processing/04macro_bench_res.sh
+++ ctrl_scripts/scripts/post_processing/04macro_bench_res.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 04macro_bench.py ~/Downloads/04_macro_bench_tf
-python3 04macro_bench.py ~/Downloads/04_macro_bench_gc
-python3 04macro_bench.py ~/Downloads/04_macro_bench_ma
-python3 04macro_bench.py ~/Downloads/04_macro_bench_mc
+python3 04macro_bench.py ~/Downloads/fastswap_logs/04_macro_bench_tf
+python3 04macro_bench.py ~/Downloads/fastswap_logs/04_macro_bench_gc
+python3 04macro_bench.py ~/Downloads/fastswap_logs/04_macro_bench_ma
+python3 04macro_bench.py ~/Downloads/fastswap_logs/04_macro_bench_mc
